8332d3da561130086fbb4fb72765af6ecec9f055
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-03-02 17:13:51 +0400

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import Header from "./components/Header"
-import Product from "./pages/Product"
-import thummbnailImg from "./assets/development.jpg"
+import Header from "./components/Header";
+import Product from "./pages/Product";
+import thummbnailImg from "./assets/development.jpg";
+import { Route, Routes, Outlet, Link } from "react-router-dom";
+import Checkout from "./pages/Checkout";
 
-
-function App() {
- 
-
+function Layout() {
   return (
     <>
-  <Header title="WEBROAD" />
-  <Product title="Web development course" description="react" images={[{src: thummbnailImg, alt: "Thumbnail image"}]} price={19.99} />
+      <Header title="WEBROAD" />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          path="/"
+          element={
+            <Product
+              title="Complete Web Development Bundle"
+              description="Learn to build websites with HTML, CSS, Javascript, React, Node, and Mongo"
+              images={[{ src: thummbnailImg, alt: "Thumbnail image" }]}
+              price={12.99}
+            ></Product>
+          }
+        />
+      </Route>
+      <Route path="checkout" element={<Checkout />} />
+        <Route path="success" element={<div>sucsess</div>} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chekout = () => {
+  return (
+    <div>
+      checkout
+    </div>
+  )
+}
+
+export default Chekout
