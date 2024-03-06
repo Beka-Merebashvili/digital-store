bb1eef2ee7aaa3762bd45e391ebd6a1a4a96b8bd
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-03-06 15:29:05 +0400

    add checkout form

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,33 @@
 import ProductCard from "../components/ProductCard"
 import PRODUCT from "../productinfo"
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  Elements,
+} from '@stripe/react-stripe-js';
+import { CheckoutForm } from '../components/CheckoutForm'
 
+type Props = {}
 
-const Chekout = () => {
+const options = {
+  mode: 'payment',
+  amount: 1099,
+  currency: 'usd',
+  // Fully customizable with appearance API.
+  appearance: {
+    /*...*/
+  },
+};
+
+const Checkout = (props: Props) => {
+  const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
+
   return (
-    <div className='flex container mt-8'>
-     <ProductCard {...PRODUCT} />
+    <div className='flex container mt-8'><ProductCard {...PRODUCT} />
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm />
+      </Elements>
     </div>
   )
 }
 
-export default Chekout
+export default Checkout
